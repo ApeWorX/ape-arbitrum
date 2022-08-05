e120fb4fab51da48d07d84b52b73109ba49d5f3a
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-08-05 10:37:36 -0400

    chore: update setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,10 +20,3 @@ def Contract():
     from ape import Contract
 
     return Contract
-
-
-def test_basic(accounts, networks):
-
-    with networks.arbitrum.local.use_provider("test"):
-        a = accounts.test_accounts[0]
-        a.transfer(a, 100)
