e40ea674be887f0a2aa0609da0e118dfe2b9a8ed
unparalleled-js <yingthi@live.com>
2022-10-07 18:05:19 -0500

    fix: add missing configs

--- ape_arbitrum/ecosystem.py
+++ ape_arbitrum/ecosystem.py
@@ -23,9 +23,27 @@ class ApeArbitrumError(ApeException):
     """
 
 
+def _create_network_config(
+    required_confirmations: int = 1, block_time: int = 1, **kwargs
+) -> NetworkConfig:
+    # Helper method to isolate `type: ignore` comments.
+    return NetworkConfig(
+        required_confirmations=required_confirmations, block_time=block_time, **kwargs
+    )  # type: ignore
+
+
+def _create_local_config(default_provider: Optional[str] = None) -> NetworkConfig:
+    return _create_network_config(
+        required_confirmations=0, block_time=0, default_provider=default_provider
+    )
+
+
 class ArbitrumConfig(PluginConfig):
-    mainnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
-    local: NetworkConfig = NetworkConfig(default_provider="test")  # type: ignore
+    mainnet: NetworkConfig = _create_network_config()
+    mainnet_fork: NetworkConfig = _create_local_config()
+    goerli: NetworkConfig = _create_network_config()
+    goerli_fork: NetworkConfig = _create_local_config()
+    local: NetworkConfig = _create_local_config(default_provider="test")
     default_network: str = LOCAL_NETWORK_NAME
 
 
