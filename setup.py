06bd34fde5906b09604bf442d97494ac53f6535f
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-10-12 15:25:18 -0500

    chore: bump ape core 0.5.2

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     url="https://github.com/ApeWorX/ape-arbitrum",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.5.0,<0.6",
+        "eth-ape>=0.5.2,<0.6",
     ],
     python_requires=">=3.8,<4",
     extras_require=extras_require,
