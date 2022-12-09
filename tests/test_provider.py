d323294fe913e5114f30c37763b0387c22c4cdc3
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-12-09 14:52:29 -0800

    test: added test for receipt (#8)

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -5,3 +5,11 @@ def test_basic(accounts, networks):
 
         assert not receipt.failed
         assert receipt.value == 100
+
+
+def test_get_receipt(accounts, networks):
+    with networks.arbitrum.local.use_provider("test"):
+        transfer = accounts.test_accounts[0].transfer(accounts.test_accounts[1], 1)
+        assert transfer.txn_hash
+        tx = networks.provider.get_receipt(transfer.txn_hash)
+        assert tx.data.hex()
